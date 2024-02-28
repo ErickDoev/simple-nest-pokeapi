0a7c36003d2f85e18f8aad813fa0ecad012568f9
elliutCruz <erick.cruz@finaktiva.com>
2024-02-28 14:02:43 -0600

    [fix] se guardan pokemos en bd

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,20 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokemonResponse } from './interfaces/pokemon-response.interface';
+import { PokemonService } from 'src/pokemon/pokemon.service';
 
 @Injectable()
 export class SeedService {
+  constructor(private pokemonService: PokemonService) {}
   private readonly axios: AxiosInstance = axios;
 
   async executeSeed() {
-    const { data } = await axios.get<PokemonResponse>(
+    const { data } = await this.axios.get<PokemonResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=10',
     );
+    const pokemons = [];
     data.results.forEach(({ name, url }) => {
       const segments = url.split('/');
       const no: number = +segments[segments.length - 2];
-      console.log({ no, name });
+      pokemons.push({ no, name });
+      this.pokemonService.create({ no, name });
     });
+
     return data.results;
   }
 }
